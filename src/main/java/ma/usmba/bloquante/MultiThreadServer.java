449687ce7bccfbd23994cd0e18c58b21133a3d56
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-15 20:41:17 +0100

    socket-multi-thread

---
+++ src/main/java/ma/usmba/bloquante/MultiThreadServer.java
@@ -0,0 +1,59 @@
+package ma.usmba.bloquante;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadServer extends Thread {
+    int clientsCount;
+    public static void main(String[] args) {
+        new MultiThreadServer().start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Server running in port : 1234");
+        try {
+            ServerSocket serverSocket=new ServerSocket(1234);
+            while (true){
+                Socket socket = serverSocket.accept();
+                ++clientsCount;
+                new Conversation(socket, clientsCount).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class Conversation extends Thread {
+        private Socket socket;
+        public int clientId;
+
+        public Conversation(Socket socket, int clientId){
+            this.socket = socket;
+            this.clientId = clientId;
+        }
+
+        @Override
+        public void run() {
+            try {
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter printWriter=new PrintWriter(outputStream,true);
+                String ip = socket.getRemoteSocketAddress().toString();
+                System.out.println("New Client Connection => "+clientId+" IP :"+ip);
+                printWriter.println("Welcome Client ID : "+clientId);
+                while (true){
+                    String request=bufferedReader.readLine();
+                    System.out.println("New Request IP :"+ip+" Request= "+request);
+                    String response="Size = "+request.length();
+                    printWriter.println(response);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/main/java/ma/usmba/bloquante/MyTelnetClient.java
@@ -0,0 +1,35 @@
+package ma.usmba.bloquante;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyTelnetClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 1234);
+            InputStream inputStream=socket.getInputStream();
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+            OutputStream outputStream=socket.getOutputStream();
+            PrintWriter printWriter=new PrintWriter(outputStream,true);
+            new Thread(()->{
+                while (true){
+                    try {
+                        String response=bufferedReader.readLine();
+                        System.out.println(response);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+            Scanner scanner=new Scanner(System.in);
+            while (true){
+                String request = scanner.nextLine();
+                printWriter.println(request);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
